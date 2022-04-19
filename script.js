b22574737206b87f1f514c31ee7de10368b225ae
Shawn Aguilar <shawn.aguilar@thermocalinc.com>
2022-04-19 16:25:14 +0000

    new changes

--- script.js
+++ script.js
@@ -12,7 +12,3 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }      
-function x()
-{
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}
