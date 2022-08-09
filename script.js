cc7180ec501887c4698e4515e472da9110cf2e09
stanky-code <shawn.aguilar@thermocalinc.com>
2022-08-09 01:13:29 -0700

    Final project submission.

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-function compute()
-{   // programs variables needed to complete logic and calculations.
+function compute() {   // programs variables needed to complete logic and calculations.
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     // Compute button output logic
-    document.getElementById("result").innerHTML = `If you deposit ${principal},<br> at an interest rate of ${rate}%.<br> You will receive an amount of ${interest},<br> in the year ${year}<br>`
-}
-//Updates interest rate based on slider position.
-function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-}
+    if (principal <= 0) {
+        alert('Enter a positive number.')
+    }else{
+        document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br> at an interest rate of <mark>${rate}</mark>%.<br> You will receive an amount of <mark>${interest}</mark>,<br> in the year ${year}<br>`
 
